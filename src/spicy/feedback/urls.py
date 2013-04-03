eec62b623de294c287f5ae73116276f45b91f6ce
Alexander Burtsev <eburus@gmail.com>
2013-04-03 13:14:19 +0400

    refs #439

--- src/spicy/feedback/urls.py
+++ src/spicy/feedback/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls.defaults import *
 
 public_urls = patterns(
-    'feedback.views',
+    'spicy.feedback.views',
     url(r'^new/$', 'new_feedback', name='new-feedback'),
 
 )
 
 admin_urls = patterns(
-    'feedback.admin',
+    'spicy.feedback.admin',
     url(r'^messages/$', 'messages', name='index'),
     url(r'^messages/(?P<message_id>\d+)/$', 'message_details', name='message-details'),
     url(r'^messages/delete/$', 'messages_delete', name='message-delete'),
