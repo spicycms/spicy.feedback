89ad14583d06d899eb18319efd7e237b24149441
Denkin Alex <avd-84@yandex.ru>
2015-04-29 15:01:13 +0300

    fix nexmomessage

--- setup.py
+++ setup.py
@@ -39,10 +39,7 @@ setup(
     namespace_packages=['spicy', ],
     install_requires=[
         'django-bitfield==1.6.4',
-        'nexmomessage==dev',
-    ],
-    dependency_links=[
-        'http://github.com/marcuz/libpynexmo/tarball/master#egg=nexmomessage-dev'
+        'nexmomessage',
     ],
 
     classifiers=[
