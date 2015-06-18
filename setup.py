c7adde72c427eae6df0583bca4ce77e6dd47f5f1
Alexandr Zolotarev <az@bramabrama.com>
2015-06-18 09:39:11 +0300

    nexmomessage

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     namespace_packages=['spicy', ],
     install_requires=[
         'django-bitfield==1.6.4',
-        'nexmomessage',
+        'git+https://github.com/marcuz/libpynexmo.git#egg=nexmomessage',
     ],
 
     classifiers=[
