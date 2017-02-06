4f9ef99ea9cec99366749fa95ce16b4ad95f56ad
Alexander Zolotarev <megabogus84@gmail.com>
2017-02-06 13:24:59 +0300

    send to from pattern email

--- src/spicy/feedback/backends/email.py
+++ src/spicy/feedback/backends/email.py
@@ -158,7 +158,7 @@ class Feedback(base.Feedback):
                 send_to = to_emails
         mail = EmailMessage(
             subject=subject, body=body,
-            from_email=self.email or settings.DEFAULT_FROM_EMAIL, to=send_to)
+            from_email=self.pattern.from_email or settings.DEFAULT_FROM_EMAIL, to=send_to)
 
         try:
             mail.send()
